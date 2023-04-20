5872ee53a76af048e199caa45154478467218fa6
Walter Silva <108630688+waltersilva23@users.noreply.github.com>
2023-04-20 17:50:34 -0300

    Update history_screen.dart
    Botão de fechar o modal

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -150,11 +150,20 @@ class _HistoryScreenState extends State<HistoryScreen>
             onTap: () {
               showDialog(
                 context: context,
-                builder: (context) => const AlertDialog(
-                  title: Center(
+                builder: (context) => AlertDialog(
+                  title: const Center(
                     child: Text('COMPROVANTE'), 
                   ),
-                  content: Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
+                  content: const Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+                  ),
+                  actions: [
+                    TextButton( // botão que fecha o modal
+                      onPressed: () => Navigator.pop(context),
+                      child: const Center (
+                        child: Text('Fechar'),
+                      ),
+                    ),
+                  ],
                 ),
               );
             },
